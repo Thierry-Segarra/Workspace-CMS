24d97419507516440f3575018d4830a2cdb6f694
Thierry-Segarra <Thierrysegarra130140@gmail.com>
2022-02-22 21:58:13 +0100

    22/02/2022 - 2
    Petit ajoue de code pour le traitement

--- SRC_creator_function/liste-creation.js
+++ SRC_creator_function/liste-creation.js
@@ -3,7 +3,7 @@ function addListeDiv(i){
    // compo = le numerot du composan
     let divcomp = "div"+i+"comp"+compo; // ex: div1comp1
     console.log(divcomp);
-    let add = '<div id="div'+i+'comp'+compo+'"><ul id="listdiv'+i+compo+'"></ul></div>';
+    let add = '<div id="div'+i+'comp'+compo+'"><ul id="listdiv'+i+compo+'"><li id="lignelistdiv'+i+(compo+1)+'">Nouvelle ellement</li></ul></div>';
     
     // Permet de créé un text dans une div
     document.getElementById('idiv'+i).innerHTML =  document.getElementById('idiv'+i).innerHTML + add;
@@ -11,7 +11,8 @@ function addListeDiv(i){
 
     tablediv["idiv"+i][divcomp] ='LISTE'; // ajouter a divcomp un nom
     console.log(tablediv);
-    compo = compo + 1;
+    compo = compo + 2; // car nous ajouton en plus une ligne dans la liste
+    
 
     boucle_composans(i);
 }
--- SRC_creator_function/traitement_ligneliste.js
+++ SRC_creator_function/traitement_ligneliste.js
@@ -1,6 +1,6 @@
 function contenue_Liste(i){
     document.getElementById('affichage_avance').style.visibility = 'visible';
-    document.getElementById("affichage_avance").innerHTML = document.getElementById("affichage_avance").innerHTML + adv_op_ligne_liste; // Permet d'affichet la liste des ellements d'une liste ou d'une tableau
+    document.getElementById("affichage_avance").innerHTML = adv_op_ligne_liste; // Permet d'affichet la liste des ellements d'une liste ou d'une tableau
     traitement_ligneliste(i);
 }
 
@@ -10,5 +10,21 @@ function traitement_ligneliste(i){
     article = article.substring(article.search('<ul id="listdiv'+i));
     article = article.substring(0,article.search('</ul>')+5);
     console.log(article);
+    
+    if(article.search('<li id="') != 0){
+        article = article.substring(article.search('<li id="')+8);
+        console.log(article);
+
+        id_ligne = article.substring(0,article.search('">')); // Pour récuperer l'id du la ligne pour identifier la ligne lors de la modification
+        console.log(id_ligne);
+
+        // isoler le nom de la lisgne pour l'affichage de la liste
+        nom_ligne = article.substring(article.search('">')+2);
+        nom_ligne = nom_ligne.substring(0,nom_ligne.search('</li>'));
+        console.log(nom_ligne);
+
+        // list_composans mettre une balsie p ou autre et mettre le nom de la ligne
+        // rajouter des bouton pour la modification, supresion, monté ou décendre la ligne
+    }
     
 }
