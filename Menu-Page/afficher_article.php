b43a5c3df013d89c51ca1ce12cccc0928d886b6b
Thierry-Segarra <Thierrysegarra130140@gmail.com>
2022-03-15 19:50:22 +0100

    15/03/2022 - note dev

--- Menu-Page/afficher_article.php
+++ Menu-Page/afficher_article.php
@@ -5,12 +5,13 @@
         <title>WORKSPACE</title>
         <link rel="stylesheet" href="../Style.css">
     </head>
-
+    <body class="article">
     <?php 
     if (isset($_GET['article'])) {   
         $article = $_GET['article'];
         include('.'.$article);
     }
     ?>
+    </body>
 
 </html>
