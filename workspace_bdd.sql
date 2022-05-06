9ff1cf22d5286a6cd93cd068a1b6e4ea9cb606f9
Thierry-Segarra <Thierrysegarra130140@gmail.com>
2022-05-06 12:22:38 +0200

    06/05/2022 BDD

--- workspace_bdd.sql
+++ workspace_bdd.sql
@@ -2,10 +2,10 @@
 -- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 04, 2022 at 10:26 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.11
+-- Hôte : 127.0.0.1
+-- Généré le : ven. 06 mai 2022 à 12:21
+-- Version du serveur :  10.4.14-MariaDB
+-- Version de PHP : 7.4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `workspace_bdd`
+-- Base de données : `workspace_bdd`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `article`
+-- Structure de la table `article`
 --
 
 CREATE TABLE `article` (
@@ -38,19 +38,10 @@ CREATE TABLE `article` (
   `Chemain` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `article`
---
-
-INSERT INTO `article` (`id`, `auteur`, `date`, `titre`, `description`, `categorie`, `Structure`, `Chemain`) VALUES
-(45, 'admin', '2022-05-04 09:20:41', 'Article-1', 'list , image et vidéo', 1, 'a:2:{i:0;a:1:{i:0;a:1:{s:5:\"idiv1\";a:6:{i:0;a:1:{s:9:\"div1comp1\";s:5:\"TITRE\";}i:1;a:1:{s:9:\"div1comp2\";s:4:\"TEXT\";}i:2;a:1:{s:9:\"div1comp4\";s:4:\"TEXT\";}i:3;a:1:{s:9:\"div1comp3\";s:5:\"LISTE\";}i:4;a:1:{s:9:\"div1comp7\";s:4:\"TEXT\";}i:5;a:1:{s:9:\"div1comp8\";s:5:\"IMAGE\";}}}}i:1;a:1:{i:0;a:1:{s:5:\"idiv2\";a:2:{i:0;a:1:{s:10:\"div2comp11\";s:5:\"TITRE\";}i:1;a:1:{s:10:\"div2comp12\";s:5:\"VIDEO\";}}}}}', './Article/Article-1.php'),
-(47, 'admin', '2022-05-04 09:36:15', 'Article-2', 'voir map', 5, 'a:1:{i:0;a:1:{i:0;a:1:{s:5:\"idiv1\";a:4:{i:0;a:1:{s:9:\"div1comp2\";s:4:\"TEXT\";}i:1;a:1:{s:9:\"div1comp1\";s:4:\"TEXT\";}i:2;a:1:{s:9:\"div1comp2\";s:4:\"TEXT\";}i:3;a:1:{s:9:\"div1comp3\";s:3:\"MAP\";}}}}}', './Article/Article-2.php'),
-(48, 'admin', '2022-05-04 09:42:48', 'Article-3', 'redirection plus téléchargement', 1, 'a:1:{i:0;a:1:{i:0;a:1:{s:5:\"idiv1\";a:4:{i:0;a:1:{s:9:\"div1comp1\";s:5:\"TITRE\";}i:1;a:1:{s:9:\"div1comp3\";s:6:\"BOUTON\";}i:2;a:1:{s:9:\"div1comp2\";s:6:\"BOUTON\";}i:3;a:1:{s:9:\"div1comp3\";s:6:\"BOUTON\";}}}}}', './Article/Article-3.php');
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `categorie`
+-- Structure de la table `categorie`
 --
 
 CREATE TABLE `categorie` (
@@ -59,17 +50,16 @@ CREATE TABLE `categorie` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `categorie`
+-- Déchargement des données de la table `categorie`
 --
 
 INSERT INTO `categorie` (`id`, `nom`) VALUES
-(1, 'Non-Defini'),
-(5, 'article2');
+(1, 'Non-Defini');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `media`
+-- Structure de la table `media`
 --
 
 CREATE TABLE `media` (
@@ -79,19 +69,10 @@ CREATE TABLE `media` (
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `media`
---
-
-INSERT INTO `media` (`id`, `type`, `nom`, `date`) VALUES
-(30, 'IMAGE', '20220504092937backgroud1.jpg', '2022-05-04'),
-(32, 'VIDEO', 'ZA6HhzlTHNE', '2022-05-04'),
-(33, 'DOCUMENT', '20220504094415CV_Thierry_Segarra.pdf', '2022-05-04');
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `page`
+-- Structure de la table `page`
 --
 
 CREATE TABLE `page` (
@@ -106,67 +87,60 @@ CREATE TABLE `page` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `page`
---
-
-INSERT INTO `page` (`id`, `auteur`, `date`, `titre`, `description`, `categorie`, `Structure`, `Chemain`) VALUES
-(19, 'admin', '2022-05-04 09:24:00', 'Page-1', '', 1, 'a:2:{i:0;a:1:{i:0;a:1:{s:5:\"idiv1\";a:3:{i:0;a:1:{s:9:\"div1comp1\";s:5:\"TITRE\";}i:1;a:1:{s:9:\"div1comp2\";s:4:\"TEXT\";}i:2;a:1:{s:9:\"div1comp3\";s:7:\"ARTICLE\";}}}}i:1;a:1:{i:0;a:1:{s:5:\"idiv2\";a:2:{i:0;a:1:{s:9:\"div2comp5\";s:4:\"TEXT\";}i:1;a:1:{s:9:\"div2comp6\";s:7:\"ARTICLE\";}}}}}', './Page/Page-1.php');
-
---
--- Indexes for dumped tables
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `article`
+-- Index pour la table `article`
 --
 ALTER TABLE `article`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `categorie`
+-- Index pour la table `categorie`
 --
 ALTER TABLE `categorie`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `media`
+-- Index pour la table `media`
 --
 ALTER TABLE `media`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `page`
+-- Index pour la table `page`
 --
 ALTER TABLE `page`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `article`
+-- AUTO_INCREMENT pour la table `article`
 --
 ALTER TABLE `article`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
--- AUTO_INCREMENT for table `categorie`
+-- AUTO_INCREMENT pour la table `categorie`
 --
 ALTER TABLE `categorie`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `media`
+-- AUTO_INCREMENT pour la table `media`
 --
 ALTER TABLE `media`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
--- AUTO_INCREMENT for table `page`
+-- AUTO_INCREMENT pour la table `page`
 --
 ALTER TABLE `page`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
